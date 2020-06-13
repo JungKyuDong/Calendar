3bcfdcba00e0993d2e78ddc8fd2627c4fc0fbfa1
ukd102 <ukd102@JUNGKYUDONG>
2020-06-13 22:37:05 +0900

    calendar (not auto version)

--- src/dennis/calendar/Calendar.java
+++ src/dennis/calendar/Calendar.java
@@ -2,13 +2,55 @@ package dennis.calendar;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+		private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,30, 31};
+		private static final int[] Leap_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,30, 31}; // 윤년
 		
-		System.out.println(" 일   월   화   수  목   금   토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		public boolean isLeapYear(int year) {
+			if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+		public int getMaxDaysOfMonth(int year, int month) {
+			if(isLeapYear(year)) {
+				return Leap_MAX_DAYS[month - 1];
+			} else {
+				return MAX_DAYS[month - 1];
+			}
+		}
+		
+		public void printCalendar(int year, int month, int weekday) {
+			
+			System.out.printf("    <<%4d년%3d월>>\n", year, month);
+			System.out.println(" SU MO TU WE TH FR SA");
+			System.out.println("--------------------");
+			
+			// print black space
+			for(int i = 0; i < weekday; i++) { // weekday : 0 ~ 6
+				System.out.print("   ");
+			}
+			int maxDay = getMaxDaysOfMonth(year, month);	
+			int count = 7 - weekday;
+			int delim = (count < 7) ? count : 0;
+			
+			// print first line
+			for(int i = 1; i <= count; i++) {
+				System.out.printf("%3d", i);
+			}
+				System.out.println();
+			
+			count++;	
+			
+			// print from second line to last
+			for(int i = count; i <= maxDay; i++) {
+				System.out.printf("%3d", i);
+				if(i % 7 == delim)
+					System.out.println();
+			}
+			System.out.println();
+			System.out.println();
 	}
 }
---
+++ src/dennis/calendar/Prompt.java
@@ -0,0 +1,77 @@
+package dennis.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+	
+	public int parseDay(String week)	{
+		if(week.equals("SU")) {
+			return 0;
+		}
+		
+		else if(week.equals("MO")) {
+			return 1;
+		}
+		else if(week.equals("TU")) {
+			return 2;
+		}
+		else if(week.equals("WE")) {
+			return 3;
+		}
+		else if(week.equals("TH")) {
+			return 4;
+		}
+		else if(week.equals("FR")) {
+			return 5;
+		}
+		else if(week.equals("SA")) {
+			return 6;
+		}
+		else
+			return 0;
+	}
+	
+	public void runPrompt() {
+		Scanner sc = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		
+		int month = 1;
+		int year = 2020;
+		int weekday = 0;
+		
+		while(true) {
+			System.out.println("년도를 입력하세요(exit : -1).");
+			System.out.print("YEAR> ");
+			year = sc.nextInt();
+			if(year == -1) {
+				break;
+			}
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			month = sc.nextInt();
+			System.out.println("첫째 날의 요일을 입력하세요(SU MO TU WE TH FR SA).");
+			String str_weekday = sc.next();
+			weekday = parseDay(str_weekday);
+			
+			if(month > 12 || month < 1) {
+				System.err.println("잘못된 입력입니다.");
+				continue;
+			}
+			
+			cal.printCalendar(year, month, weekday);
+			System.out.println();
+		}
+		
+		System.out.println("Bye~");
+		sc.close();
+	}
+	
+	public static void main(String[] args) {
+		
+		// 셀 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+			
+
+	}
+}
